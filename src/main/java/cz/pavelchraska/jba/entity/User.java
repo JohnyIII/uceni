cd0da781bcc21e853c5988b60b2d0c64ffd24e6e
Michal Pavlusek <pavlusek@datalite.cz>
2016-11-03 15:38:19 +0100

    Commit Praha 1

--- src/main/java/cz/pavelchraska/jba/entity/User.java
+++ src/main/java/cz/pavelchraska/jba/entity/User.java
@@ -30,6 +30,31 @@ public class User {
     @OneToMany(mappedBy = "user")
     private List<Blog> blogs;
 
+
+    @Override
+    public int hashCode()
+    {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        return super.equals(obj);
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException
+    {
+        return super.clone();
+    }
+
+    @Override
+    public String toString()
+    {
+        return super.toString();
+    }
+
     public List<Blog> getBlogs() {
         return blogs;
     }
